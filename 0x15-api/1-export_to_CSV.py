0faaa02062d10f29032480d550277e9afb0930da
Motlamelle <lebomotlamelle@gmail.com>
2022-10-22 18:23:47 +0200

    add files

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,25 +9,31 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    BASE_URL = "https://jsonplaceholder.typicode.com"
     employee_id = sys.argv[1]
-    resp = requests.get('{}/users/{}/'.format(BASE_URL, employee_id))
+    resp = requests.get("{}/users/{}/".format(BASE_URL, employee_id))
 
     if resp.status_code == 200:
         employee = json.loads(resp.text)
-        resp = requests.get('{}/todos?userId={}'.format(BASE_URL, employee_id))
+        resp = requests.get("{}/todos?userId={}".format(BASE_URL, employee_id))
         todos = json.loads(resp.text)
-        with open('{}.csv'.format(str(employee_id)), 'w') as csvfile:
+        with open("{}.csv".format(str(employee_id)), "w") as csvfile:
             row = {
-                'user_id': '',
-                'username': '',
-                'task_completed_status': '',
-                'task_title': ''
+                "user_id": "",
+                "username": "",
+                "task_completed_status": "",
+                "task_title": "",
             }
-            writer = csv.DictWriter(csvfile, fieldnames=list(row.keys()), quotechar='"', quoting=csv.QUOTE_ALL)
+            writer = csv.DictWriter(
+                csvfile,
+                fieldnames=list(row.keys()),
+                quotechar='"',
+                quoting=csv.QUOTE_ALL,
+                lineterminator="\n",
+            )
             for todo in todos:
-                row['task_completed_status'] = todo['completed']
-                row['task_title'] = todo['title']
-                row['user_id'] = employee['id']
-                row['username'] = employee['username']
+                row["task_completed_status"] = todo["completed"]
+                row["task_title"] = todo["title"]
+                row["user_id"] = employee["id"]
+                row["username"] = employee["username"]
                 writer.writerow(row)
